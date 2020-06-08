e5303b9f9cce3a7eba3721b6379772bc0a88f7b5
miracle-E <50963919+miracle-E@users.noreply.github.com>
2020-06-08 17:06:11 +0100

    user model

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 'use strict'
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+mongoose.connect('mongodb://localhost/hng');
+let User = require('./models/user')
+
+let db = mongoose.connection;
+
+db.once('open',()=>{
+    console.log('connected to Mongodb');
+});
+
+// check for db error
+db.on('error', ()=>{
+    console.log(err);
+});
+
+// init app
 const app = express();
+
+// body-parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+app.get('/', (req,res)=>{
+    res.send('Welcome to Team-granite Api')
+});
 
 // listen on port 
 app.listen(2000, function(){
---
+++ models/user.js
@@ -0,0 +1,25 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let userSchema =  Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true  
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    }
+});
+
+let User = mongoose.model('User', userSchema);
+
+module.exports = {User};
