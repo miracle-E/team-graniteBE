e895c4700e77821f2438bf2f0744e41045ceee23
Ekom FInbarr <ekfinbarr@gmail.com>
2020-06-09 06:05:26 +0000

    Error handler middleware added

--- src/database/index.js
+++ src/database/index.js
@@ -2,15 +2,21 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/hng');
 
 let db = mongoose.connection;
-
-db.once('open',()=>{
-    console.log('connected to Mongodb');
+try {
+    db.once('open',()=>{
+        console.log('connected to Mongodb');
+        console.log("##############################\n");
+    });
+} catch (error) {
+    console.error('Mongodb connection error!');
     console.log("##############################\n");
-});
+}
+
 
 // check for db error
-db.on('error', ()=>{
-    console.log(err);
+db.on('error', (err)=>{
+    console.error('Mongodb: ', err);
+    console.log("##############################\n");
 });
 
 module.exports = db;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,38 @@
 'use strict'
-const express = require('express');
-const bodyParser = require('body-parser');
-let User = require('./models/user')
-const db = require("./database/index");
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const User = require('./models/user')
+const db = require('./database/index')
+const handleErrors = require('./middleware/handleErrors')
+// const { BadRequest } = require('./utils/errors')
 
 // init app
-const app = express();
+const app = express()
+const port = 2000
 
 // body-parser middleware
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
+app.get('/', (req, res) => {
+  res.send('Welcome to Team-granite Api')
+})
 
-app.get('/', (req,res)=>{
-    res.send('Welcome to Team-granite Api')
-});
+// error handler middleware
+app.get('*', function (req, res, next) {
+  // Reporting async errors *must* go through `next()`
+  setImmediate(() => {
+    next(new Error())
+  })
+})
 
-// listen on port 
-app.listen(2000, function(){
-    console.log("\n##############################")
-    console.log('Server started on port 2000')
-});
+app.use(handleErrors)
 
-module.exports = app;
+// listen on port
+app.listen(port, function () {
+  console.log('\n##############################')
+  console.log(`Listening at http://localhost:${port}\n`)
+})
+
+module.exports = app
